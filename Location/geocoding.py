6b843e6b9bd94191392f9445bdbf13eda46378d7
Jorge A. Toro <jolthgs@gmail.com>
2013-06-24 16:20:25 -0500

    update

--- Location/geocoding.py
+++ Location/geocoding.py
@@ -42,10 +42,9 @@
 """
 import urllib, json
 
-#GEOCODE_BASE_URL = 'http://maps.google.com/maps/api/geocode/json'
 GEOCODE_BASE_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
 REGEOCODE_BASE_URL = 'http://maps.google.com/maps/geo'
-OSM_REGEOCODE_BASE_URL = 'http://nominatim.openstreetmap.org/reverse'
+
 
 def geocodeGMap(address, sensor='false', **geo_args):
     """
@@ -110,28 +109,4 @@ def regeocodeGMap(latlng, level=None, sensor='false', **geo_args):
                for s in result['results']]))[0]
 
 
-def regeocodeOSM(latlng, **geo_args):
-    """
-        OSM Reverse Geocoding
-
-        usage:
-        >>> geocoding.regeocodeOSM('5.03515676667,-75.4606770833')
-        u'50, Parque Industrial Juanchito, Tesorito, Comuna Tesorito, Manizales, Centrosur, Caldas, 170003, Colombia'
-        >>>
-    """
-    latlng = latlng.split(',')
-
-    geo_args = ({
-        'format' : 'json',
-        'lat'  : latlng[0],
-        'lon' : latlng[1],
-        'zoom'  : 18,
-        'addressdetails'  : 1,
-        'email'  : 'jorge.toro@devmicrosystem.com'.encode("utf8")
-    })
-
-    url = OSM_REGEOCODE_BASE_URL + '?' + urllib.unquote(urllib.urlencode(geo_args))
-    result = json.load(urllib.urlopen(url))
-
-    return result['display_name']
 
